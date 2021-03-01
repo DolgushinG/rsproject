fd361a9ca0cf61e3217bdc51aba7298249e8d879
Georgii Dolgushin <g.dolgushin@mygento.ru>
2021-03-02 00:04:33 +0300

    fix new

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -13,7 +13,7 @@
           <li><a class="nav-link scrollto" href="{{route('about')}}">About</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#team"></a></li>
           <li><a href="{{route('blog')}}">Blog</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
@@ -21,18 +21,14 @@
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
                   <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
                 </ul>
               </li>
               <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="{{route('contact')}}">Contact</a></li>
+          <li><a class="nav-link scrollto" href="{{route('contact')}}">Связаться с нами</a></li>
+          <li><a class="nav-link scrollto" href="{{route('register')}}">Регистрация</a></li>
+          <li><a class="nav-link scrollto" href="{{route('login')}}">Вход</a></li>
           <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
