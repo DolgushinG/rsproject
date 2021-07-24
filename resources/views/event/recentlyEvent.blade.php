14241de1afe7e624d8e1f2148b8986f4e8b53a42
dolgushinG <dolgushing@yandex.ru>
2021-07-24 13:32:48 +0300

    fixed recenltyevent

--- resources/views/event/recentlyEvent.blade.php
+++ resources/views/event/recentlyEvent.blade.php
@@ -12,7 +12,7 @@
             <div class="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="100">
                 <div class="box">
                     <h3>{{$event->event_title}}</h3>
-                    <div class="price">{{date('m', strtotime($event->event_start_date))}}<span> / {{date('d', strtotime($event->event_start_date))}}</span></div>
+                    <div class="price">{{date('d', strtotime($event->event_start_date))}}<span>/{{date('m', strtotime($event->event_start_date))}}/{{date('y', strtotime($event->event_start_date))}}</span></div>
                     <img src="{{asset('storage'.$event->event_image)}}" class="img-fluid" alt="">
                     <ul>
                         <li>{{$event->event_description}}</li>
