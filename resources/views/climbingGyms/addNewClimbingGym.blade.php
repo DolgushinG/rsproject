7c2317467fb29ee7a6e38b308bb942c0b45cf177
dolgushinG <dolgushing@yandex.ru>
2021-08-08 21:12:16 +0300

    added climbing gyms fixed 3

--- resources/views/climbingGyms/addNewClimbingGym.blade.php
+++ resources/views/climbingGyms/addNewClimbingGym.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     <link href="https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/css/suggestions.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/js/jquery.suggestions.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.1.60/inputmask/jquery.inputmask.js"></script>
 
     <!-- ======= Hero Section ======= -->
