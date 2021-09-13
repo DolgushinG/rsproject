e6f36132c16158208202f3589aa7d05c0cfd2f36
dolgushinG <dolgushing@yandex.ru>
2021-09-13 23:55:40 +0300

    fixed moves

--- resources/views/climbingMoves/moves.blade.php
+++ resources/views/climbingMoves/moves.blade.php
@@ -27,7 +27,22 @@
 
 <script>
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        document.querySelectorAll('video').forEach((e)=> {e.setAttribute('autoplay','')})
+        $(window).scroll(function(e)
+        {
+            var offsetRange = $(window).height() / 3,
+                offsetTop = $(window).scrollTop() + offsetRange + $("#header").outerHeight(true),
+                offsetBottom = offsetTop + offsetRange;
+
+            $("video").each(function () {
+                var y1 = $(this).offset().top;
+                var y2 = offsetTop;
+                if (y1 + $(this).outerHeight(true) < y2 || y1 > offsetBottom) {
+                    this.pause();
+                } else {
+                    this.play();
+                }
+            });
+        });
     }
 </script>
 
