73fb894680fe65233a0a49df763148b0c27c070d
Georgii Dolgushin <g.dolgushin@mcplat.ru>
2022-10-03 16:07:41 +0300

    fixed sandwich for android device

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -269,8 +269,19 @@
     <!-- End #main -->
     @include('sponsors.sponsors')
     <script>
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-            document.querySelectorAll('video').forEach((e)=> {e.setAttribute('autoplay','')})
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        // Windows Phone must come first because its UA also contains "Android"
+        // if (/windows phone/i.test(userAgent)) {
+        //     return "Windows Phone";
+        // }
+
+        if (/android/i.test(userAgent)) {
+            document.getElementById("navbar").classList.add('android');
         }
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            document.getElementById("navbar").classList.remove('android');
+        }
+        document.querySelectorAll('video').forEach((e)=> {e.setAttribute('autoplay','')})
     </script>
 @endsection
