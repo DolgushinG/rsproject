e418ef3ff5bf06f90af4e46ecf6a8b194da67690
Georgii Dolgushin <g.dolgushin@mcplat.ru>
2022-10-05 23:01:17 +0300

    auto changed status article published

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ use App\Console\Commands\SendInfo;
 use App\Console\Commands\SendMail;
 use App\Mail\NewInfo;
 use App\Mail\NewUser;
+use App\Models\Posts;
 use Illuminate\Support\Facades\Artisan;
 use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
@@ -46,6 +47,20 @@ class Kernel extends ConsoleKernel
                 User::whereNull('email_verified_at')->delete();
             }
         })->weekly();
+        $schedule->call(function () {
+            $post = Posts::where('status', '=', 'PENDING')->first();
+            if ($post){
+                $post->status = 'PUBLISHED';
+                $post->save();
+                $details = [
+                    'title' => "Schedule publish post",
+                    'body' => date('Y-m-d H:i:s'),
+                    'userName' => $post->title,
+                    'userCity' => $post->status
+                ];
+                Mail::to('Dolgushing@yandex.ru')->send(new NewInfo($details));
+            }
+        })->dailyAt('10:00');
         $schedule->exec("php artisan backup:run --only-db")->weeklyOn(1, '00:00');
         $schedule->exec("php artisan backup:run")->weeklyOn(2, '00:00');
          // Backups (to Google Drive)
