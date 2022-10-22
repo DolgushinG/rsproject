803e6b7ad33be78beba11f3e1163e9596e4591e2
Georgii Dolgushin <g.dolgushin@mcplat.ru>
2022-10-22 23:46:32 +0300

    fixed style

--- resources/views/climbingGyms/votesGyms.blade.php
+++ resources/views/climbingGyms/votesGyms.blade.php
@@ -1,5 +1,5 @@
 @foreach ($likesDislikesGyms as $gym)
-    <li class="search_city list-group-item d-flex justify-content-between align-items-center table-hover" style="color:white!important;">
+    <li class="search_city list-group-item d-flex justify-content-between align-items-center table-hover" >
         {{$gym->name}}<span class="badge badge-primary badge-pill" title="Количество лайков"
                             data-toggle="tooltip"
                             data-placement="bottom">{{$gym->sum_likes}}</span>
